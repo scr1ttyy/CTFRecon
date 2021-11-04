0812c8a2eece0dbebe69f12ee6f94bdf6c61f1a1
0xf00d <hambyhaxx@gmail.com>
2021-11-04 18:36:23 +0800

    updated README

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Todo:
 - [ ] Fix ugly code.
 - [ ] Make input interchangeable.
 - [ ] Have error checks in input.
+
+Socials:
+
+- [Twitter](https://twitter.com/hambyhaxx)
+
+Donation box:
+
+- [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/hambyhaxx)
+- [BuyMeACoffee](https://www.buymeacoffee.com/hambyhaxx)
--- ctfrecon.sh
+++ ctfrecon.sh
@@ -3,7 +3,7 @@
 # User Input
 IP=$1
 DIR_NAME=$2
-
+PLATFORM=$3
 # Create directory inputted by user.
 mkdir $DIR_NAME
 cd $DIR_NAME/
@@ -12,6 +12,8 @@ cd $IP/
 mkdir loot scans ss exploit
 clear
 echo "Successfully created $DIR_NAME directory!"
+echo "$IP ${DIR_NAME}.htb" >> /etc/hosts
+echo "Successfully added ${DIR_NAME}.${PLATFORM} to /etc/hosts"
 
 # Run scanning scripts
 echo "Now Scanning..."
@@ -19,6 +21,6 @@ echo "Log files saved at scans/ directory!"
 nmap -T4 -A -p- -oN scans/${DIR_NAME}_nmap_scan.txt $IP >/dev/null
 wait
 echo "Nmap scan: Finished"
-gobuster dir -u http://$IP -w /usr/share/seclists-git/Discovery/Web-Content/directory-list-2.3-medium.txt -t 64 -o scans/${DIR_NAME}_GoBuster_scan.txt &>/dev/null
+gobuster dir -u http://${DIR_NAME}.${PLATFORM} -w /usr/share/seclists-git/Discovery/Web-Content/directory-list-2.3-medium.txt -t 64 -o scans/${DIR_NAME}_GoBuster_scan.txt &>/dev/null
 wait
 echo "GoBuster Directory Scan: Finished"
